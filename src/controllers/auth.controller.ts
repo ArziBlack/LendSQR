49a32bbbe353d9ddf050bd3b273ee85abe8f279b
Milton Black <arziblack2@gmail.com>
2024-06-28 15:09:18 +0100

    added user data payload to response

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -26,7 +26,7 @@ export const login = async (req: Request, res: Response): Promise<Response> => {
   try {
     const token = await jwt.sign({ email: credential.email }, process.env.SECRET_KEY as string, { expiresIn: "7d" });
     console.log(colors.rainbow("Login Successful"));
-    return res.status(200).json({ message: "Login Successful", token });
+    return res.status(200).json({ message: "Login Successful", user: credential, token });
   } catch (error) {
     console.error(colors.underline.red("Error generating token"), error);
     return res.status(500).json({ message: "Error generating token" });
